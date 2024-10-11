3b21facf22d2813a9a847f943620297a59afca23
Kian <kian.leiner@gmail.com>
2024-10-11 10:40:16 -0700

    Optimize imports

--- src/main/java/com/kleinercode/fabric/deathmessages/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/kleinercode/fabric/deathmessages/mixin/ServerPlayerEntityMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
