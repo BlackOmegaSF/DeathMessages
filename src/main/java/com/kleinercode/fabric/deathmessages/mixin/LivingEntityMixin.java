04c3de33818c4271ef6ac8d7d97c35bdd83b4991
Kian <kian.leiner@gmail.com>
2025-03-16 13:17:50 -0700

    Remove Shadow of nonexistent method

--- src/main/java/com/kleinercode/fabric/deathmessages/mixin/LivingEntityMixin.java
+++ src/main/java/com/kleinercode/fabric/deathmessages/mixin/LivingEntityMixin.java
@@ -4,15 +4,12 @@ import com.kleinercode.fabric.deathmessages.OnLivingEntityDeathCallback;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-
-    @Shadow public abstract boolean shouldDropXp();
 
     @Inject(method = "onDeath", at = @At("HEAD"))
     private void onDeath(DamageSource source, CallbackInfo info) {
